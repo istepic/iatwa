60bfe27f1198964798a8554372f31bf682daf789
istepic <imiste@pm.me>
2020-12-18 21:00:51 +0100

    Fixed wrong sdk path

--- prepare_build.sh
+++ prepare_build.sh
@@ -3,7 +3,7 @@
 cmake                                                     \
    -DCMAKE_TOOLCHAIN_FILE=cmake/arm-none-eabi.cmake       \
    -DTOOLCHAIN_PREFIX=/home/istep/gcc-arm-none-eabi-9-2020-q2-update \
-   -DNRF5_SDK_PATH=~/diplomski_rad_DK/nrf_sdk_16          \
+   -DNRF5_SDK_PATH=~/iatwa_DK/nrf_sdk_16          \
    -DNRF5_BOARD=pca10040                                  \
    -DNRF5_SOFTDEVICE_VARIANT=s132                         \
    -DNRF5_SDKCONFIG_PATH=config                           \
