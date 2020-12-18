bd84835150d07f162671aa1f93b44c2307b92d9f
istepic <imiste@pm.me>
2020-12-18 20:49:31 +0100

    Renamed

--- prepare_build.sh
+++ prepare_build.sh
@@ -7,7 +7,7 @@ cmake                                                     \
    -DNRF5_BOARD=pca10040                                  \
    -DNRF5_SOFTDEVICE_VARIANT=s132                         \
    -DNRF5_SDKCONFIG_PATH=config                           \
-   -DNRF5_LINKER_SCRIPT=config/diplomski_rad.ld           \
+   -DNRF5_LINKER_SCRIPT=config/iatwa.ld                   \
    -G "Unix Makefiles"                                    \
    -B build                                               \
    -S .
